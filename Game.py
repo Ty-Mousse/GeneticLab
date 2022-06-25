e4ade90f77bd3be365dee6ff6b0f6fe5d77247e8
Clément Rault <clem.rault@outlook.com>
2022-06-25 20:42:30 +0200

    Wall setting

--- Game.py
+++ Game.py
@@ -1,6 +1,9 @@
+from ctypes import sizeof
 import pygame
 import sys
 import math
+
+from urllib3 import Retry
 from Player import Player
 
 # Constantes
@@ -25,13 +28,14 @@ class Game():
 
         # Initialisation de la carte
         self.map, self.map_size = self.getMap("map.txt")
+        # Instanciation des murs
+        self.walls = self.getWalls()
 
         # Initialisation des joueurs
         self.player = Player(24, 24)
 
         # Boucle de jeu
         self.start()
-
 
     def getMap(self, filename):
             f = open(filename, 'r')
@@ -43,6 +47,35 @@ class Game():
                 cpt += 1
             return map, cpt
 
+    def getWalls(self):
+        wall_list = []
+        for row in range(self.map_size):
+            for column in range(self.map_size):
+                index = row*self.map_size + column
+                l_index_x = row*self.map_size + column - 1
+                n_index_x = row*self.map_size + column + 1
+                l_index_y = (row - 1)*self.map_size + column 
+                n_index_y = (row + 1)*self.map_size + column
+                if self.map[index] == '#':
+                    if (l_index_x > 0) and (l_index_x < 1024) and (self.map[l_index_x] == ' '):
+                        wall_list.append(((column*TILE_SIZE, row*TILE_SIZE, column*TILE_SIZE, (row + 1)*TILE_SIZE)))
+                    if (n_index_x > 0) and (n_index_x < 1024) and (self.map[n_index_x] == ' '):
+                        wall_list.append((((column + 1)*TILE_SIZE, row*TILE_SIZE, (column + 1)*TILE_SIZE, (row + 1)*TILE_SIZE)))
+                    if (l_index_y > 0) and (l_index_y < 1024) and (self.map[l_index_y] == ' '):
+                        wall_list.append(((column*TILE_SIZE, row*TILE_SIZE, (column + 1)*TILE_SIZE, row*TILE_SIZE)))
+                    if (n_index_y > 0) and (n_index_y < 1024) and (self.map[n_index_y] == ' '):
+                        wall_list.append(((column*TILE_SIZE, (row + 1)*TILE_SIZE, (column + 1)*TILE_SIZE, (row + 1)*TILE_SIZE)))
+        return(wall_list)
+
+    def getIntercectionPoint(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        num = (x1 - x3)*(y1 - y2) - (y1 - y3)*(x1 - x2)
+        denum = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+        u = num/denum
+        if (0 <= u) and (u <= 1):
+            inter_pt = (x3 + u*(x4 - x3), y3 + u*(y4 - y3))
+            return True, inter_pt
+        else:
+            return False, (x2, y2)
 
     def drawMap(self):
         # Dessin de la carte
@@ -64,13 +97,27 @@ class Game():
                     )
         # Dessin du joueur
         pygame.draw.circle(self.window, (255, 0, 0), (self.player.x, self.player.y), 5)
-        pygame.draw.line(
-                self.window,
-                (0, 0, 255),
-                (self.player.x, self.player.y),
-                (self.player.x + math.sin(self.player.angle)*self.player.max_ray_length,
-                self.player.y + math.cos(self.player.angle)*self.player.max_ray_length)
-            )
+        for (x1, y1, x2, y2) in self.walls:
+            test, pi = self.getIntercectionPoint(
+                    self.player.x,
+                    self.player.y,
+                    self.player.x + math.sin(self.player.angle)*self.player.max_ray_length,
+                    self.player.y + math.cos(self.player.angle)*self.player.max_ray_length,
+                    x1,
+                    y1,
+                    x2,
+                    y2
+                )
+            print(test, pi)
+            if (test):
+                pygame.draw.line(
+                        self.window,
+                        (0, 0, 255),
+                        (self.player.x, self.player.y),
+                        pi
+                    )
+                break
+            
         for i in range(self.player.nb_ray//2):
             pygame.draw.line(
                 self.window,
@@ -90,7 +137,6 @@ class Game():
     def clearMap(self):
         # Clear de la fenêtre
         self.window.fill((0, 0, 0))
-
 
     def start(self):
         while True:
